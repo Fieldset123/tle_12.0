5d1a5ffeb1f3174a3a343509b8ffa8599222ebae
Fieldset123 <134182816+Fieldset123@users.noreply.github.com>
2025-01-09 17:25:11 +0530

    Add files via upload

---
+++ Ad-hoc/lec1/adhoc_lec1_Multiple_powers_of_two.cpp
@@ -0,0 +1,37 @@
+//problem link:- https://www.hackerrank.com/contests/dcc-lab-30-jan/challenges/multiple-powers-of-two/problem
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, q;
+    cin >> n >> q;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    long long maxi = 32;
+    while (q--)
+    {
+        long long x;
+        cin >> x;
+        if (x >= maxi)
+            continue;
+        maxi = x;
+        for (int i = 0; i < n; i++)
+        {
+            if ((a[i] & ((1 << x) - 1)) == 0)
+                a[i] += ((1 << x) - 1);
+        }
+    }
+    //  31 * n
+    for (auto it : a)
+        cout << it << " ";
+    cout << endl;
+    return 0;
+}
+
+// tc - O(31*n)
+// sc - O(n)
